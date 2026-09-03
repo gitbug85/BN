27b665dd01f4e0bfb937c7a9bf5fdb4e54a41701
gitbug85 <140214265+gitbug85@users.noreply.github.com>
2026-09-03 09:21:12 -0600

    Update build.sh

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -1,4 +1,4 @@
-nim c ../bn.nim
-rustc --crate-type=staticlib ../runtime/math.rs -o ../runtime/math.a
-rustc --crate-type=staticlib ../runtime/cli.rs -o ../runtime/cli.a
-rustc --crate-type=staticlib ../runtime/string.rs -o ../runtime/string.a
+nim c ./src/bn.nim
+rustc --crate-type=staticlib ./src/runtime/math.rs -o ./src/runtime/math.a
+rustc --crate-type=staticlib ./src/runtime/cli.rs -o ./src/runtime/cli.a
+rustc --crate-type=staticlib ./src/runtime/string.rs -o ./src/runtime/string.a
