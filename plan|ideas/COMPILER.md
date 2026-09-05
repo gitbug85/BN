f165e1b0748bf205d203b6735543f53233be989f
gitbug85 <140214265+gitbug85@users.noreply.github.com>
2026-09-05 13:01:33 -0600

    Unfinished lexer update

--- plan|ideas/COMPILER.md
+++ plan|ideas/COMPILER.md
@@ -5,9 +5,9 @@ bn.nim (controller)
 **lexer.pl** -> lexemes (json)
 1. (Lexeme) Lexeme object (col, ln)
 2. (string) Seperate by line
-3. (string) Ignore after #
-4. (string) Go character by character and reset current_segment if character is " " while making list
-5. (array) Replace segment with more segments based on regex
-6. (Lexeme array) Use switch statement for Lexeme making
+3. (string) Go character by character and reset current_segment if character is " " while making list
+4. (array) Replace segment with more segments based on regex
+5. (Lexeme array) Use switch statement for Lexeme making
+6. (string) Ignore # and after #
 **tokenizes** -> tokens ->
 **etc**
--- src/lexer.pl
+++ src/lexer.pl
@@ -1,25 +1,72 @@
 use feature 'class';
+use JSON::PP;
 no warnings 'experimental::class';
 
-class Lexeme {
+class Segment {
     field $col :param;
     field $ln  :param;
     field $val :param;
 }
 
+use constant {
+    JOINING_STRING => 0,
+    JOINING_PATH => 1,
+    NOT_JOINING => 2,
+};
+
+sub TO_JSON {
+    my $self = shift;
+    return { %$self };
+}
+
 sub lex {
     my $filename = shift;
-    my @lexemes;
+    my @fragments;
     
     open(my $fh, '<:encoding(UTF-8)', $filename) 
         or die "Could not open file '$filename': $!";
 
+    # Make fragments based on spaces
     while (my $line = <$fh>) {
         chomp $line;
-        
-        push @lexemes, Lexeme->new(col => 0, ln => 0, val => $line);
         print "Processing: $line\n";
+
+        my $current_fragment = "";
+        my @chars = split('', $line);
+        my $space = " ";
+
+        for my $i (0 .. $#chars) {
+            if ($chars[$i] == $space) {
+                if (current_fragment != "") {
+                    push @fragments, Segment->new(col => $i, ln => $., val => current_fragment);
+                    $current_fragment = "";
+                }
+                push @fragments, Segment->new(col => $i, ln => $., val => $space);
+            }
+            $current_fragment .= $chars[$i]
+        }
+        
+        push @fragments, Segment->new(col => 0, ln => $., val => $line);
     }
+
+=pod
+    Join fragments together to make lexemes:
+     - Strings ""
+     - Paths ``
+     Everything else is one to one.
+=cut
+
+    my $lexing_status = NOT_JOINING;
+    my @lexemes;
+
+    # Add code here for making lexemes
+
+    # Convert to json and return json string
+    my @plain_fragments = map { { %$_ } } @fragments; # Use fragments for now but switch to lexemes
+    my $json_encoder = JSON::PP->new;
+    my $json_string  = $json_encoder->encode(\@plain_fragments);
+
+    print $json_string, "\n";
 }
 
 my $input_file = $ARGV[0]; 
